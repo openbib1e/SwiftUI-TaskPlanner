04cf2e4458f01bedc3b1bee48a72cf4f087b11c3
openbible <open8ible@gmail.com>
2024-06-04 14:04:26 +0900

    create project

---
+++ TaskPlannerCloneCoding/TaskPlannerCloneCoding/TaskPlannerCloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerCloneCodingApp.swift
+//  TaskPlannerCloneCoding
+//
+//  Created by 편성경 on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerCloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

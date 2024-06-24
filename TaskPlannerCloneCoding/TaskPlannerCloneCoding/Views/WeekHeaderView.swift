ea745a263b5f95407e8f655cd421cc5d2c816cc4
openbible <open8ible@gmail.com>
2024-06-24 17:48:48 +0900

    20240624 WeekHeaderView 생성

---
+++ TaskPlannerCloneCoding/TaskPlannerCloneCoding/Views/WeekHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  WeekHeaderView.swift
+//  TaskPlannerCloneCoding
+//
+//  Created by 편성경 on 6/24/24.
+//
+
+import SwiftUI
+
+struct WeekHeaderView: View {
+    @ObservedObject var viewModel = TaskViewModel()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeekHeaderView()
+}
